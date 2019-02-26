ea62351868740ed3823b021c3328ea72b55c257f
adamenk <mnadamenko@gmail.com>
2019-02-26 12:50:18 +0300

    init

---
+++ src/main/java/com/adamenko/springbootreact/model/Event.java
@@ -0,0 +1,61 @@
+package com.adamenko.springbootreact.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.time.Instant;
+import java.util.Set;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Instant date;
+    private String title;
+    private String description;
+    @ManyToMany
+    private Set<User> attendees;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<User> getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(Set<User> attendees) {
+        this.attendees = attendees;
+    }
+}
